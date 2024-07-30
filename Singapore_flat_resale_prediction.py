323eeb61140dff2b078337ab44e85cda5460e29a
RajiVenkat89 <157869418+RajiVenkat89@users.noreply.github.com>
2024-07-30 08:50:16 +0530

    ch
    c

--- Singapore_flat_resale_prediction.py
+++ Singapore_flat_resale_prediction.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-from streamlit_option_menu import option_menu
 import requests
 st.set_page_config(page_title='SINGAPORE FLAT RESALE PREDICTION',layout="wide")
 url="https://raw.githubusercontent.com/RajiVenkat89/SingaporeFlatResalePrediction/main/Model/Regression_model.pkl"
